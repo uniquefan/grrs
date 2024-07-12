f5d35a72340bbb85fdaee8d38210f3ae4391d559
Alireza Safaei <alirezasafaei9494@gmail.com>
2024-07-13 01:23:19 +0330

    use match for result

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,17 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Can't deal with {}, just exit here", error);
+        }
+    };
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-
 }

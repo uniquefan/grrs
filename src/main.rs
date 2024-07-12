07cf34054c42e882271d1f133f4b5aa108f6e44b
Alireza Safaei <alirezasafaei9494@gmail.com>
2024-07-13 01:30:19 +0330

    use custom erro with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -6,19 +7,15 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => content,
-        Err(error) => {
-            panic!("Can't deal with {}, just exit here", error);
-        }
-    };
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }

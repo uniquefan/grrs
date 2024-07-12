53c4decc7e0f0823155584ba74a11e33d60f1111
Alireza Safaei <alirezasafaei9494@gmail.com>
2024-07-13 00:55:42 +0330

    get args with clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+use clap::Parser;
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
